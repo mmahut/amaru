11c6228ddd6aa18cf061ac186a5f405d765bca5e
KtorZ <matthias.benkort@gmail.com>
2026-05-30 14:52:36 +0200

    test: add phase-one validation test using preprod culprit; to rule out script integrity calculation rules mishap
    Signed-off-by: KtorZ <matthias.benkort@gmail.com>

--- crates/amaru-ledger/src/rules/transaction/phase_one/mod.rs
+++ crates/amaru-ledger/src/rules/transaction/phase_one/mod.rs
@@ -326,6 +326,7 @@ mod tests {
     #[test_case(fixture!("fail/BabbageOutputTooSmallUTxO/0"); "output below minimum lovelace")]
     #[test_case(fixture!("fail/OutputTooBigUTxO/0"); "output value larger than maxValueSize")]
     #[test_case(fixture!("fail/WrongNetworkInTxOutput/0"); "output address on wrong network")]
+    #[test_case(fixture!("pass/script-integrity-hash/0"); "interesting script integrity hash on preprod")]
     fn conformance(fixture: Fixture) {
         let tx_size = fixture.transaction.len() as u64;
 
